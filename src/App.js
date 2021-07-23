29aec70d5e58da0fc2a06e1e8b78f8a6ff700650
Ben-Elsom <79074687+Ben-Elsom@users.noreply.github.com>
2021-07-23 16:34:07 +1000

    initial commit and made call to backend API and am presenting menu

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function App() {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [menuItems, setMenuItems] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/menu")
+    .then(response => response.json())
+    .then(data => setMenuItems(data))
+
+  }, [])
+  return ( 
+  <>
+    {menuItems.map((menuItem)=>( 
+    <div key={menuItem.id}>
+        {console.log(menuItems)}
+
+        <h1>{menuItem.name}</h1>
+        <ul>
+          <li>{menuItem.description}</li>
+          <li>{menuItem.available.toString()}</li>
+          <li>{menuItem.price}</li>
+          <li>{menuItem.category_id}</li>
+        </ul>
+        <img width="200px" src={menuItem.image_url}/>
+      </div>
+    ))}
+  
+  </>
   );
 }
 
