2f4129f843aa9be446c109b2659db3f5565667c5
Ben-Elsom <79074687+Ben-Elsom@users.noreply.github.com>
2021-07-25 15:47:33 +1000

    made fetch post call but can't get it to work

--- src/components/AddItemForm.js
+++ src/components/AddItemForm.js
@@ -5,11 +5,13 @@ const AddItemForm = (props) => {
         event.preventDefault()
         const {setMenuItems, menuItems} = props
         const { target } = event 
-    s
-       
-        setMenuItems([...menuItems, {name: target[0].value, description: target[2].value, available: target[1].checked, price: target[3].value, image_url: "#"}])
+        const newMenuItem = {name: target[0].value, description: target[2].value, available: target[1].checked, price: target[3].value, image_url: "#"}
+        setMenuItems([...menuItems, newMenuItem])
+        const ret = async() => {
+            await fetch("http://localhost:5000/api/menu", {method: "POST", headers:{"Content-Type": "application/json"}, body:{name: "test"}})
+        }
+        console.log(ret())
    
-        // Make fetch/post request to http://localhost:3000/api/menu with this data
     }
     return (
         <form onSubmit={addItem}>
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,13 +1,13 @@
 const Menu = (props) => {
-  const {name, description, available, price, category_id, image_url} = props.data
+  const {name, description, available, price, category, image_url} = props.data
     return (
       <div style={{border: '2px solid red', width: '50%'}}>
               <h1>{name}</h1>
               <ul>
-                <li>{description}</li>
-                <li>{available.toString()}</li>
-                <li>{price}</li>
-                <li>{category_id}</li>
+                <li>Description: {description}</li>
+                <li>Available?: {available.toString()}</li>
+                <li>Price: {price}</li>
+                <li>Category: {category}</li>
               </ul>
               <img width="200px" src={image_url}/>
       </div>
